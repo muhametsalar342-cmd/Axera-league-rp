2e1574c45d93bb4b832e860b1ef8aa814556bd46
muhametsalar342-cmd <muhametsalar342@gmail.com>
2026-09-03 15:19:35 +0300

    Create package.js

---
+++ package.js
@@ -0,0 +1,12 @@
+{
+  "name": "axera-league-rp",
+  "version": "1.0.0",
+  "description": "Axera League Discord RP Bot",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "discord.js": "^14.22.1"
+  }
+}
